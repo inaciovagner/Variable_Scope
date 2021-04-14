5544aeb557d0b14048020943f8ef01e499916684
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-14 20:03:13 +0100

    Add project files.

---
+++ Variable_Scope/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Variable_Scope
+{
+    class Program
+    {
+        static void Write()
+        {
+            string myString = "String defined in Write()";
+            WriteLine("Now in Write()");
+            WriteLine($"myString = {myString}");
+        }
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Write();
+            WriteLine("\nNow in Main()");
+            WriteLine($"myString = {myString}");
+            ReadKey();
+        }
+    }
+}
